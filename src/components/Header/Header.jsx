4c51859a1b75ad0809201f0482fa9c13c8aaf00a
Sean Gurson <sdgurson@gmail.com>
2017-11-28 21:47:48 -0800

    setting up

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src="./images/vegies-left.jpg" alt=""></img>
+      <h1>Wholesome Foods</h1>
+      <h2>Delicious meals made with locally sourced organic ingredients</h2>
+    </div>
+  )
+}
+
+export default Header;
--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../Header/Header.jsx'
 
 const Page = () => {
   return (
     <div>
       <h1>Restaurant App Starter Template</h1>      
+      <Header/>
     </div>
   )
 }
